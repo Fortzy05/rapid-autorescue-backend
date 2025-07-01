0b129ffaa25bc8e7ca3d40d9319daeca33d80217
Fortzy05 <omorodion.fortune@gmail.com>
2025-07-01 13:51:03 +0100

    fixes

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ const app = express();
 const PORT = 5000;
 
 // ✅ Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://www.rapidautorescue.co.uk",
+  })
+);
+  
 app.use(express.json());
+
+
 
 // ✅ Setup Sanity client
 const sanity = createClient({
